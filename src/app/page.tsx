7e674eb6876a55b2058443dadd92f280f181fae8
Abhay627 <gautamabhay3009@gmail.com>
2025-10-07 23:56:22 +0530

    feat : fetch api data with SSR{tRPC server component}

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,26 @@
-"use client";
+// "use client";
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/db";
 import { useTRPC } from "@/trpc/client";
+import { caller } from "@/trpc/server";
 import { useQuery } from "@tanstack/react-query";
 //accessing data through prisma client 
 
-const Page = () => {
+const Page = async () => {
   
   // const users = await prisma.user.findMany();
   // const posts = await prisma.post.findMany();
   // console.log(users);
   // console.log(posts);
-  const trpc=useTRPC();
-  // trpc.createAI.queryOptions({text:"abhay"});
-  const {data} = useQuery(trpc.createAI.queryOptions({text:"abhay"}));
+
+  // using it with the help of trpc client component 
+  // const trpc=useTRPC();
+  
+  // const {data} = useQuery(trpc.createAI.queryOptions({text:"abhay"}));
+
+  //now fetching the data using trpc server caller function
+  const data = await caller.createAI({text:"Abhay Gautam"});
+
   return ( 
     <div>
       <Button>Hello World!</Button>
