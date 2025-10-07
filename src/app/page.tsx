8d855719c7aed5c03e4b924ac1079df55015f14b
Abhay627 <gautamabhay3009@gmail.com>
2025-10-07 22:29:06 +0530

    feat:enable tRPC client component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,25 @@
+"use client";
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/db";
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
 //accessing data through prisma client 
 
-const Page = async () => {
-  const users = await prisma.user.findMany();
-  const posts = await prisma.post.findMany();
-  console.log(users);
-  console.log(posts);
+const Page = () => {
+  
+  // const users = await prisma.user.findMany();
+  // const posts = await prisma.post.findMany();
+  // console.log(users);
+  // console.log(posts);
+  const trpc=useTRPC();
+  // trpc.createAI.queryOptions({text:"abhay"});
+  const {data} = useQuery(trpc.createAI.queryOptions({text:"abhay"}));
   return ( 
     <div>
       <Button>Hello World!</Button>
-      {JSON.stringify(users,null,2 )}
-      {JSON.stringify(posts,null,2 )}
+      <div>{JSON.stringify(data)}</div>
+      {/* {JSON.stringify(users,null,2 )}
+      {JSON.stringify(posts,null,2 )} */}
     </div>
    );
 }
