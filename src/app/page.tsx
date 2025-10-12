f71c5a83a3aa18d560020dedda7e08d1d8f73f5c
Abhay627 <gautamabhay3009@gmail.com>
2025-10-13 02:56:13 +0530

    feat:AI Integration using GROK API

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,11 @@
 import { Button } from "@/components/ui/button";
 import { prisma } from "@/lib/db";
 import { useTRPC } from "@/trpc/client";
+import React from "react";
 import {caller, getQueryClient ,trpc } from "@/trpc/server";
 import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
+import { Input } from "@/components/ui/input";
 
 
 //please remember thst only server components can be async functions 
@@ -15,9 +17,12 @@ const Page = () => {
       toast.success("Background job invoked!");
     }
   }));
+  const [value, setValue] = React.useState("");
+
   return ( 
     <div>
-      <button  disabled = {invoke.isPending} onClick ={() => invoke.mutate({ text: "john" })} className="bg-blue-500 text-white px-4 py-2 rounded">
+      <Input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Enter the command" className="mb-4" />
+      <button  disabled = {invoke.isPending} onClick ={() => invoke.mutate({ value: value })} className="bg-blue-500 text-white px-4 py-2 rounded">
         invoke background job   
       </button>
     </div>
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,17 +1,19 @@
+import { createAgent, grok } from "@inngest/agent-kit";
+
 import { inngest } from "./client";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
-    //assume this is video transcoding or some other long running task
-    await step.sleep("wait-a-moment", "30s");
-    //imagine this is some other long running task
-    await step.sleep("wait-a-moment", "20s");
-    //imagine this is image parsing or doc generation
-    await step.sleep("wait-a-moment", "10s");
+    const agent = createAgent({
+        name: "Code writer",
+        system: "You are an expert Summarizer . You summarize in two words",
+        model: grok({ model: "grok-4-latest" }),
+    });
 
+    const {output} = await agent.run(`Summarise the following text: ${event.data.value}`);
     // success message 
-    return { message: `Hello ${event.data.email}!` };
+    return {output};
   },
 );
