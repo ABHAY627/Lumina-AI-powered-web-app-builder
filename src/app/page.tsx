d1a9643f3e1ed2caf0f26707319d96b4d51db24d
Abhay627 <gautamabhay3009@gmail.com>
2025-12-22 17:58:47 +0530

    Debug the Sandbox Errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,13 @@
 "use client";
-import { Button } from "@/components/ui/button";
-import { prisma } from "@/lib/db";
 import { useTRPC } from "@/trpc/client";
 import React from "react";
-import {caller, getQueryClient ,trpc } from "@/trpc/server";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { useMutation } from "@tanstack/react-query";
 import { toast } from "sonner";
 import { Input } from "@/components/ui/input";
 
-//please remember that only server components can be async functions 
-
 const Page = () => {
-  const trpc=useTRPC();
-  const data = useQuery(trpc.createAI.queryOptions({ text: "Abhay Gautam" }));
-  const invoke = useMutation<{ success: boolean }, unknown, { value: string }>(
+  const trpc = useTRPC();
+  const invoke = useMutation(
     trpc.invoke.mutationOptions({
       onSuccess: () => {
         toast.success("Background job invoked!");
